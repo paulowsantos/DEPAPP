fe80d5d13495bcc6459172fe0ea573fa217eb0d7
Paulo Wayner <paulowsantos@gmail.com>
2020-04-10 18:49:51 -0700

    Updates

---
+++ precache-manifest.eb031434490fd8adda8792c255aaf9bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26bd74149422288f8b245520e4f0ef35",
+    "url": "/DEPAPP/index.html"
+  },
+  {
+    "revision": "43feefe49671b60c005a",
+    "url": "/DEPAPP/static/js/2.6b4040f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DEPAPP/static/js/2.6b4040f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32f65806cf1aa2a9215",
+    "url": "/DEPAPP/static/js/main.4aada38d.chunk.js"
+  },
+  {
+    "revision": "143b627e2ad61d1cc8ca",
+    "url": "/DEPAPP/static/js/runtime-main.b6a9d1d9.js"
+  }
+]);
